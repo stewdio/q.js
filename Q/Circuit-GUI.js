432694e1d7ee83a1c18ead8df985e3135df34d41
Stewart Smith <stew@rtsmith.io>
2019-11-19 17:59:11 -0500

    Updates to Circuit drag-and-drop.

--- Q/Circuit-GUI.js
+++ Q/Circuit-GUI.js
@@ -27,22 +27,36 @@ Q.Circuit.createDomMenu = function(){
 		const operation = Q.Gate.findByLabel( label )
 
 		const gateEl = document.createElement( 'div' )
-		gateEl.setAttribute( 'title', operation.name )		
+		gateEl.setAttribute( 'title', operation.name )
 		gateEl.style.gridRow    = 1
 		gateEl.style.gridColumn = i + 1
 
 		const gateSvgEl = document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' )
 		gateSvgEl.classList.add( 'qjs-circuit-gate' )
+		gateSvgEl.operation = operation
 		gateEl.appendChild( gateSvgEl )
 
 		const useEl = document.createElementNS( 'http://www.w3.org/2000/svg', 'use' )
 		useEl.classList.add( 'qjs-circuit-gate-'+ operation.css )
 		useEl.setAttributeNS( 'http://www.w3.org/1999/xlink', 'xlink:href', '#qjs-circuit-gate-'+ operation.css )
-		useEl.operation = operation
 		gateSvgEl.appendChild( useEl )
 
 		layerOperationsEl.appendChild( gateEl )
 	})
+
+	Array.from( menuEl.querySelectorAll( `
+
+		svg.qjs-circuit-gate, 
+		.qjs-circuit-moment, 
+		.qjs-circuit-register, 
+		.qjs-circuit-input` 
+
+	)).forEach( function( el ){
+		
+		el.addEventListener( 'mousedown',  Q.Circuit.GUI.grab )
+		el.addEventListener( 'touchstart', Q.Circuit.GUI.grab )
+	})
+	
 	return menuEl
 }
 
@@ -134,8 +148,8 @@ Q.Circuit.prototype.toDom = function(){
 
 			const wireSvgEl = document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' )
 			wireSvgEl.classList.add( 'qjs-circuit-wire' )
-			wireSvgEl.style.gridRow = ( o + 2 )
-			wireSvgEl.style.gridColumn = ( m + 3 )			
+			wireSvgEl.style.gridRow = o + 2
+			wireSvgEl.style.gridColumn = m + 3
 			
 			const wireUseEl = document.createElementNS( 'http://www.w3.org/2000/svg', 'use' )
 			wireUseEl.setAttributeNS( 'http://www.w3.org/1999/xlink', 'xlink:href', '#qjs-circuit-wire' )
@@ -155,19 +169,15 @@ Q.Circuit.prototype.toDom = function(){
 
 			const identitySvgEl = document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' )
 			identitySvgEl.classList.add( 'qjs-circuit-gate' )
-			identitySvgEl.operation = {//  hehehehehe.
-				
-				gate: Q.Gate.IDENTITY,
-				momentIndex: m,
-				registerIndex: o
-			}
+			identitySvgEl.operation = Q.Gate.IDENTITY
+			identitySvgEl.setAttributeNS( 'http://www.w3.org/1999/xlink', 'momentIndex', m )
+			identitySvgEl.setAttributeNS( 'http://www.w3.org/1999/xlink', 'registerIndex', o )
 			identityEl.appendChild( identitySvgEl )
 
 			const identityUseEl = document.createElementNS( 'http://www.w3.org/2000/svg', 'use' )
 			identityUseEl.setAttributeNS( 'http://www.w3.org/1999/xlink', 'xlink:href', '#qjs-circuit-gate-identity' )
 			identityUseEl.classList.add( 'qjs-circuit-gate-identity' )
-			identitySvgEl.appendChild( identityUseEl )
-						
+			identitySvgEl.appendChild( identityUseEl )			
 			layerGrabbablesEl.appendChild( identityEl)
 
 			
@@ -182,7 +192,9 @@ Q.Circuit.prototype.toDom = function(){
 
 				const gateSvgEl = document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' )
 				gateSvgEl.classList.add( 'qjs-circuit-gate' )
-				gateSvgEl.operation = operation//  hehehehehe.
+				gateSvgEl.operation = operation
+				gateSvgEl.setAttributeNS( 'http://www.w3.org/1999/xlink', 'momentIndex', m )
+				gateSvgEl.setAttributeNS( 'http://www.w3.org/1999/xlink', 'registerIndex', o )
 				gateEl.appendChild( gateSvgEl )
 
 				const useEl = document.createElementNS( 'http://www.w3.org/2000/svg', 'use' )
@@ -244,6 +256,31 @@ Q.Circuit.prototype.toDom = function(){
 	})
 
 
+
+
+	//  Add events to our circuit.
+
+	Array.from( circuitEl.querySelectorAll( `
+
+		svg.qjs-circuit-gate, 
+		.qjs-circuit-moment, 
+		.qjs-circuit-register, 
+		.qjs-circuit-input` 
+
+	)).forEach( function( el ){
+		
+		el.addEventListener( 'mousedown',  Q.Circuit.GUI.grab )
+		el.addEventListener( 'touchstart', Q.Circuit.GUI.grab )
+	})
+	Array.from( circuitEl.querySelectorAll( '.qjs-circuit-layer > *' )).forEach( function( el ){
+
+		el.addEventListener( 'mouseover', Q.Circuit.GUI.highlight )		
+	})
+	circuitEl.addEventListener( 'mouseout', Q.Circuit.GUI.unhighlight )
+
+
+
+
 	//  All done. Return our document fragment.
 
 	return circuitEl
@@ -255,201 +292,271 @@ Q.Circuit.prototype.toDom = function(){
 
 
 
-/*
-
-
-https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
-
-https://mdn.github.io/dom-examples/drag-and-drop/copy-move-DataTransfer.html
-
-https://mdn.github.io/dom-examples/drag-and-drop/copy-move-DataTransferItemList.html
-
-*/
-
-let
-grabbedItem = null
 
 
 
 
+Q.Circuit.GUI = {
 
-const
-highlight = function( event ){
+	grabbedItem: null,
+	highlight: function( event ){
 
-	let cellEl = event.target
-	while( cellEl.parentNode && cellEl.tagName.toLowerCase() !== 'div' ){
-		
-		cellEl = cellEl.parentNode
-	}
+		let cellEl = event.target
+		while( cellEl.parentNode && cellEl.tagName.toLowerCase() !== 'div' ){
+			
+			cellEl = cellEl.parentNode
+		}
 
-	let layerEl = event.target
-	while( layerEl.parentNode && !layerEl.classList.contains( 'qjs-circuit-layer' )){
+		let layerEl = event.target
+		while( layerEl.parentNode && !layerEl.classList.contains( 'qjs-circuit-layer' )){
 
-		layerEl = layerEl.parentNode
-	}
+			layerEl = layerEl.parentNode
+		}
 
-	let circuitEl = layerEl.parentNode
-
-	const 
-	style  = getComputedStyle( cellEl ),
-	selectedRow    = parseFloat( style.gridRowStart ),
-	selectedColumn = parseFloat( style.gridColumnStart )
-
-	Array.from( circuitEl.querySelectorAll( '.qjs-circuit-layer-grabbables *, .qjs-circuit-layer-wires *' )).forEach( function( el ){
+		let circuitEl = layerEl.parentNode
 
 		const 
-		style = getComputedStyle( el ),
-		thisRow    = parseFloat( style.gridRowStart ),
-		thisColumn = parseFloat( style.gridColumnStart )
+		style  = getComputedStyle( cellEl ),
+		selectedRow    = parseFloat( style.gridRowStart ),
+		selectedColumn = parseFloat( style.gridColumnStart )
 
-		if(
-			( thisRow > 1 || thisColumn > 1 ) && (//  Don’t highlight the menu area.
+		Array.from( circuitEl.querySelectorAll( '.qjs-circuit-layer-grabbables *, .qjs-circuit-layer-wires *' )).forEach( function( el ){
+
+			const 
+			style = getComputedStyle( el ),
+			thisRow    = parseFloat( style.gridRowStart ),
+			thisColumn = parseFloat( style.gridColumnStart )
+
+			if(
+				( thisRow > 1 || thisColumn > 1 ) && (//  Don’t highlight the menu area.
 
 
-				( selectedRow === 1 && thisColumn === selectedColumn ) ||//  If on a moment label, only highlight its column.
-				( selectedColumn < 3 && thisRow === selectedRow      ) ||//  If on a register label, only highlight its row.
-				( 
-					selectedRow > 1 && 
-					selectedColumn > 2 && 
-					( thisRow === selectedRow || thisColumn === selectedColumn )
+					( selectedRow === 1 && thisColumn === selectedColumn ) ||//  If on a moment label, only highlight its column.
+					( selectedColumn < 3 && thisRow === selectedRow      ) ||//  If on a register label, only highlight its row.
+					( 
+						selectedRow > 1 && 
+						selectedColumn > 2 && 
+						( thisRow === selectedRow || thisColumn === selectedColumn )
+					)
 				)
-			)
-		){
+			){
 
-			el.classList.add( 'qjs-circuit-highlight' )
-		}
-		else {
+				el.classList.add( 'qjs-circuit-highlight' )
+			}
+			else {
 
-			el.classList.remove( 'qjs-circuit-highlight' )
-		}
-	})
-},
-unhighlight = function( event ){
-
-
-	let circuitEl = event.target
-	while( circuitEl.parentNode && !circuitEl.classList.contains( 'qjs-circuit' )){
-
-		circuitEl = circuitEl.parentNode
-	}
-	Array.from( circuitEl.querySelectorAll( '.qjs-circuit-layer-grabbables *, .qjs-circuit-layer-wires *' )).forEach( function( el ){
-
-		el.classList.remove( 'qjs-circuit-highlight' )
-	})
-},
-grab = function( event ){
-	
-	event.preventDefault()
-	event.stopPropagation()
-	grabbedItem = event.target
-	// console.log( 'grabbed', event )
-	console.log( 'grabbing this', grabbedItem.operation )
-},
-move = function(){
+				el.classList.remove( 'qjs-circuit-highlight' )
+			}
+		})
+	},
+	unhighlight: function( event ){
 
 
-},
-drop = function( event ){
-
-	if( grabbedItem !== null ){
-	
-		console.log(event.target)
-
-		let dropTarget = event.target
-		while( dropTarget.classList && !dropTarget.classList.contains( 'qjs-circuit-gate' )){
-
-			dropTarget = dropTarget.parentNode
-		}
-		
 		let circuitEl = event.target
 		while( circuitEl.parentNode && !circuitEl.classList.contains( 'qjs-circuit' )){
 
 			circuitEl = circuitEl.parentNode
 		}
-		const circuit = circuitEl.circuit
+		Array.from( circuitEl.querySelectorAll( '.qjs-circuit-layer-grabbables *, .qjs-circuit-layer-wires *' )).forEach( function( el ){
+
+			el.classList.remove( 'qjs-circuit-highlight' )
+		})
+	},
+	grab: function( event ){
+		
+		event.preventDefault()
+		event.stopPropagation()
+
+		
+		//  We might be grabbing directly on the USE element
+		//  or more broadly on the SVG element (where the operation is stored).
+		//  Either way we’ll find the operation we’re intending to drag!
+
+		let grabbedItem = event.target
+		while( grabbedItem.parentNode && grabbedItem.operation === undefined ){
+
+			grabbedItem = grabbedItem.parentNode
+		}
+		Q.Circuit.GUI.grabbedItem = grabbedItem
+	},
+	move: function(){
 
 
+	},
+	drop: function( event ){
 
-		if( dropTarget.classList && dropTarget.classList.contains( 'qjs-circuit-gate' )){
-
-			const 
-			operationToInsert  = grabbedItem.operation,
-			operationToReplace = dropTarget.operation
-			
-			console.log( 'operationToInsert', operationToInsert )
-			console.log( 'operationToReplace', operationToReplace )
-			
-			console.log( 'REMOVE ops at moment #', operationToReplace.momentIndex, 'containing register #', operationToReplace.registerIndex )			
-			console.log( 'and replace with: ', operationToInsert.label )
-			// console.log( 'on circuit:', circuit )
-
-			circuit.set$( operationToReplace.momentIndex + 1, Q.Gate.findByLabel( operationToInsert.label ), [ operationToReplace.registerIndex ])
-			console.log( circuit.toDiagram() )
-			console.log( circuit.evaluate$() )
-			console.log( circuit.report$() )
+		if( Q.Circuit.GUI.grabbedItem !== null ){
 
 
-			
-			//  ********************************************************************************
-			//  well this is stupid and temporary!
-			//  must come back and fix / make generic / use events?
-			//  and also need to remove event listeners, all that stuff
-			//  because WOW this is just bad practice!
-			//  but the demo works ;)
+			//  What element are we dropping on to?
+			//  Note that it may not be the event’s target element,
+			//  if may be an ancestor node.
 
-			if( circuit === bellPlus ){
+			let receivingEl = event.target
+			while( receivingEl.parentNode && 
+				receivingEl.operation === undefined ){
 
-				const parent = circuitEl.parentNode
-				parent.removeChild( circuitEl )
-				parent.appendChild( circuit.toDom() )
-
-			
-				document.getElementById( 'bell-plus-report' ).innerText = bellPlus.report$()
+				receivingEl = receivingEl.parentNode
 			}
-			//  ********************************************************************************
-		
+			
+
+			//  If our receiving element is a Qjs Circuit Gate,
+			//  then we’re in business!
+
+			if( receivingEl.classList && 
+				receivingEl.classList.contains( 'qjs-circuit-gate' )){
+				
+
+				//  What element did we drag and drop on to here?
+				//  Note that these momentIndex and registerIndex attributes
+				//  may be undefined. That’s ok! 
+				//  That just means there’s no “cut” operation to perform after.
+				//  Also note that storing the momentIndex as a tag attribute
+				//  is redudant because it is indeed contained within operation.
+				//  However -- operation does NOT contain the cell’s registerIndex!
+				//  It instead contains a registerIndices Array, remember? ;)
+				//  And that may contain multiple elements; some unrelated to this cell!
+				//  So we must store the registerIndex in this way.
+				
+				const 
+				grabbedEl = Q.Circuit.GUI.grabbedItem,
+				grabbedMomentIndex   = +grabbedEl.getAttribute( 'momentIndex' ),
+				grabbedRegisterIndex = +grabbedEl.getAttribute( 'registerIndex' )
+
+
+				//  What operation did we drag on to here?
+				//  And what operation will we be replacing?
+				
+				const 
+				operationToInsert  = grabbedEl.operation,
+				operationToReplace = receivingEl.operation
+				
+
+				//  What cell have we dropped on to?
+				
+				const
+				receivingMomentIndex   = +receivingEl.getAttribute( 'momentIndex' ),
+				receivingRegisterIndex = +receivingEl.getAttribute( 'registerIndex' )
+
+
+				//  We expect that the containing DIV for the circuit
+				//  is an ancestor of the receiving element.
+				//  Our actual Q.Circuit reference will be attached to that!
+
+				let circuitEl = receivingEl
+				while( circuitEl.parentNode && 
+					!circuitEl.classList.contains( 'qjs-circuit' )){
+
+					circuitEl = circuitEl.parentNode
+				}
+				const circuit = circuitEl.circuit
 
 
 
+
+
+				console.log( 'operationToInsert',  operationToInsert )
+				console.log( 'operationToReplace', operationToReplace )
+				
+				console.log( 'REMOVE op at moment #', receivingMomentIndex, ', register #', receivingRegisterIndex )			
+				console.log( 'and replace with: ', operationToInsert.label )
+				// console.log( 'on circuit:', circuit )
+
+
+
+
+				//  x
+
+				circuit.set$(
+
+					receivingMomentIndex + 1, 
+					//operationToInsert,
+					Q.Gate.findByLabel( operationToInsert.label ),
+					[ receivingRegisterIndex ]
+				)
+
+
+				//  Do we need to find the circuit that grabbedEl came from?
+
+				if( Q.isUsefulNumber( grabbedMomentIndex ) && 
+					Q.isUsefulNumber( grabbedRegisterIndex )){
+
+					
+					//  Find the circuit that grabbedEl came from.
+
+					let grabbedFromCircuitEl = grabbedEl
+					while( grabbedFromCircuitEl.parentNode && 
+						!grabbedFromCircuitEl.classList.contains( 'qjs-circuit' )){
+
+						grabbedFromCircuitEl = grabbedFromCircuitEl.parentNode
+					}
+					const grabbedFromCircuit = grabbedFromCircuitEl.circuit
+
+					if( grabbedFromCircuit ){
+
+
+						//  REMOVE the operation from this circuit!
+						//***********
+					}
+
+				}
+
+
+
+				//operationToInsert.momentIndex = operationToReplace.momentIndex
+				//operationToInsert.registerIndex = operationToReplace.registerIndex
+
+
+				// console.log( circuit.toDiagram() )
+				// console.log( circuit.evaluate$() )
+				// console.log( circuit.report$() )
+
+
+
+				
+				//  ********************************************************************************
+				//  well this is stupid and temporary!
+				//  must come back and fix / make generic / use events?
+				//  and also need to remove event listeners, all that stuff
+				//  because WOW this is just bad practice!
+				//  but the demo works ;)
+
+				if( circuit === bellPlus ){
+
+					const parent = circuitEl.parentNode
+					parent.removeChild( circuitEl )
+					parent.appendChild( circuit.toDom() )
+					circuit.evaluate$()
+				
+					document.getElementById( 'bell-plus-report' ).innerText = bellPlus.report$()
+					document.getElementById( 'bell-plus-diagram' ).innerText = bellPlus.toDiagram()
+					document.getElementById( 'bell-plus-text' ).innerText = bellPlus.toText()
+				}
+				//  ********************************************************************************
+			
+
+
+
+			}
+			else {
+
+				console.log( 'dropped on to NOTHING' )
+			}
+			Q.Circuit.GUI.grabbedItem = null
 		}
-		else {
-
-			console.log( 'dropped on to NOTHING' )
-		}
-		grabbedItem = null
 	}
-},
-addEvents = function( el = document.body ){
-
-	Array.from( el.querySelectorAll( 'svg.qjs-circuit-gate, .qjs-circuit-moment, .qjs-circuit-register, .qjs-circuit-input' )).forEach( function( el ){
-
-		
-		el.addEventListener( 'mousedown', grab )
-		el.addEventListener( 'touchstart', grab )
-
-		// el.addEventListener( 'mouseup', drop )
-		// el.addEventListener( 'touchend', drop )
-	})
-	document.addEventListener( 'mouseup', drop )
-	document.addEventListener( 'touchend', drop )
+}
 
 
 
 
 
 
-	Array.from( document.querySelectorAll( '.qjs-circuit-layer > *' )).forEach( function( el ){
 
-		el.addEventListener( 'mouseover', highlight )		
-	})
-	Array.from( document.querySelectorAll( '.qjs-circuit' )).forEach( function( el ){
+const addEvents = function( el = document.body ){
 
-		el.addEventListener( 'mouseout', unhighlight )
-	})
-
-
-
-	//  need to add an  unhighlight for mouseout of any circuit.
+	document.addEventListener( 'mouseup',  Q.Circuit.GUI.drop )
+	document.addEventListener( 'touchend', Q.Circuit.GUI.drop )
 }
 
 
