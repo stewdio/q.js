b476e931e8415dc490abed4f9a49bf1487f79b04
Stefan Seegerer <manza@outlook.de>
2021-02-07 15:19:32 +0100

    Update Q-Circuit-Editor.js

--- Q/Q-Circuit-Editor.js
+++ Q/Q-Circuit-Editor.js
@@ -1369,7 +1369,7 @@ Q.Circuit.Editor.onPointerPress = function( event ){
 			console.log('→ Edit input Qubit value at', registerIndex)
 			circuit.qubits[registerIndex - 1] = 
 				circuit.qubits[registerIndex - 1].applyGate(Q.Gate.PAULI_X)
-			let qbitLabel = document.querySelector(
+			let qbitLabel = circuitEl.querySelector(
 				".Q-circuit-header.Q-circuit-input[register-index='" +
 				  registerIndex +
 				  "']"
